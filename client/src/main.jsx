0de2e39941993aa7d789360ad0188803cdfeb797
iverial <ivan.caggiano61@gmail.com>
2023-12-13 20:03:33 -0300

    fix bug deploy front

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,11 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import dotenv from "dotenv";
 import axios from 'axios';
+import dotenv from "dotenv";
 dotenv.config();
 
-
-// eslint-disable-next-line no-undef
-axios.defaults.baseURL =  process.env.REACT_APP_API || 'http://localhost:3001';
+axios.defaults.baseURL = import.meta.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
