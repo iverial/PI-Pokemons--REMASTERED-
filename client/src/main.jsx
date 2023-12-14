7174c132faf2c8c0c41a06b40c2a2bb02db92061
iverial <ivan.caggiano61@gmail.com>
2023-12-13 21:07:46 -0300

    bug fix css and other things

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,9 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import dotenv from "dotenv";
 import axios from 'axios';
-dotenv.config();
-
-
-// eslint-disable-next-line no-undef
-axios.defaults.baseURL =  process.env.REACT_APP_API || 'http://localhost:3001';
+// console.log('REACT API', import.meta.env.REACT_APP_API_URL)
+axios.defaults.baseURL = import.meta.env.VITE_REACT_APP_API_URL
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
